9f21d9d4dba12544b662d766f8cbb0fda8793310
vaganov-spb <vaganov.te@phystech.edu>
2019-12-02 12:56:39 +0300

    HW7 v1.1

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
