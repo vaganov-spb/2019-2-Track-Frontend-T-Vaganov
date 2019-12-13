2ee276315a904aa4e9dd87dc71ab9484ea38fa9f
vaganov-spb <vaganov.te@phystech.edu>
2019-12-13 21:36:13 +0300

    HW8 v1.1

--- src/components/ButtonsChat.js
+++ src/components/ButtonsChat.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { Chat } from './MessageForm';
 import messageStyles from '../styles/MessageForm.module.css';
