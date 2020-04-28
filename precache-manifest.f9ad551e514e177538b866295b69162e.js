80d664038b8077557a8c69e4db6fbc3cb47f5e4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 12:29:37 +0000

    Deploy vaganov-spb/2019-2-Track-Frontend-T-Vaganov to github.com/vaganov-spb/2019-2-Track-Frontend-T-Vaganov.git:gh-pages

---
+++ precache-manifest.f9ad551e514e177538b866295b69162e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3be097c6145b63ea16cda08200e536",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/index.html"
+  },
+  {
+    "revision": "44fdb0304b9cf56327ad",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/css/2.911d384e.chunk.css"
+  },
+  {
+    "revision": "e060088cef61822f747a",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/css/main.a204c7a8.chunk.css"
+  },
+  {
+    "revision": "44fdb0304b9cf56327ad",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/js/2.d35232bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/js/2.d35232bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e060088cef61822f747a",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/js/main.e50485ce.chunk.js"
+  },
+  {
+    "revision": "66c79dd0e878082a43da",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/js/runtime-main.9c62103f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/2019-2-Track-Frontend-T-Vaganov/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
